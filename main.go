436de08c13df0b1e5bdec18433d584c2476a6b7c
John Nguyen <jtnguyen236@gmail.com>
2020-06-19 12:09:39 +0800

    add funcs

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+
+}
+
+// Result of the Add
+type Result struct {
+	Sum int
+}
+
+// Add x and y together
+func Add(x int, y int) *Result {
+	return &Result{x + y}
+}
